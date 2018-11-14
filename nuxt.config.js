0ad79ddcb936ec496038001b4737d648d8f4aa0a
uni-ww <1043269839@qq.com>
2018-11-14 17:44:13 +0800

    配置nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,70 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'keywords', name: 'keywords', content: 'nuxt-demo' },
-      { name: 'format-detection', content: 'telephone=no, email=no, date=no, address=no' },
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description },
+            { hid: 'keywords', name: 'keywords', content: 'nuxt-demo' },
+            { name: 'format-detection', content: 'telephone=no, email=no, date=no, address=no' },
+            { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+            { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon1.ico' },
+            { rel: 'stylesheet', type: 'text/css', href: '/css/base.css' },
+        ],
+        script:[
+            {type: 'text/jacascript', src:'/js/rem.js'}
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#333' },
+
+    /*
+     ** Global CSS
+     */
+    css: [
+        '~/assets/css/common.less'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon1.ico' }
-    ]
-  },
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+    ],
 
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Build configuration
-  */
-  build: {
-  }
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://github.com/nuxt-community/axios-module#usage
+        '@nuxtjs/axios',
+    ],
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        prefix: '/api/',
+        proxy: true
+    },
+    proxy: {
+        '/api/': { target: 'http://admin.chinakingding.com/', pathRewrite: {'^/api/': ''} }
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        vendor: ['axios']
+    }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,35 @@
         <h2>hhh</h2>
         <div class="box">this is box</div>
         <div class="test">
-            <div class="t-box"></div>
+            <div class="t-box bor-1px-b">this is test</div>
         </div>
     </section>
 </template>
 <script>
 export default {
-    components: {}
+    components: {},
+    asyncData(context) {
+        // console.log(context.$axios.get)
+        // console.log(app.$axios)
+
+        context.$axios.post('api/Index/web_index')
+        .then(res => {
+            cosnole.log(res)
+        })
+    }
 }
+
 </script>
 <style lang="less">
-@import url('../assets/less/style.less');
+@import url('../assets/css/style.less');
 
 .container {
     min-height: 100vh;
 
     .box {
-        color: @cl-r;
+        .fsize(.4rem);
+        color: @cl-3;
+        .bor-1px(red);
     }
 }
 
